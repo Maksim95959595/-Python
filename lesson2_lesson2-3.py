7cbcf572c92ec80d10f84b75cbb5f1020d783445
Maksim95959595 <123316473+Maksim95959595@users.noreply.github.com>
2023-01-22 20:26:20 +0400

    Update lesson2_lesson2-3.py

--- lesson2_lesson2-3.py
+++ lesson2_lesson2-3.py
@@ -1,9 +1,3 @@
-# Задача №3
-# Пользователь вводит месяц в виде целого числа от 1 до 12.
-# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
-# Напишите решения через list и через dict.
-
-# Решение через список с защитой от неверного ввода,до тех пор пока не введут правильное значение
 month = int(input("Please enter month id from 1 to 12 : "))
 mlist = ["зима", "весна", "лето", "осень"]
 while True:
@@ -24,12 +18,3 @@ while True:
     elif month >= 9 and month < 12:
         print(f"\tSeason related to your Month id# {month} is '{mlist[3]}'")
         break
-
-# Решение через словарь
-seasons = {1: "зима", 2: "весна", 3: "лето", 4: "осень"}
-months_dict = {1: seasons.get(1), 2: seasons.get(1), 3: seasons.get(2), 4: seasons.get(2), 5: seasons.get(2),
-               6: seasons.get(3), 7: seasons.get(3), 8: seasons.get(3), 9: seasons.get(4), 10: seasons.get(4),
-               11: seasons.get(4), 12: seasons.get(1)}
-value = months_dict[month] if month in months_dict else print(f"Your month id {month} is not found in seasons")
-print(f"\tSearch result in seasons dictionary for month id# {month} is:  '{value}'")
-
